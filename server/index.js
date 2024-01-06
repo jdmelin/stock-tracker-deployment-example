5caf3c8513ef42159003db7462d5a04da2fd35a4
Jonathan Melin <melin.jonathan@gmail.com>
2024-01-06 14:15:21 -0500

    Update config details

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
 const http = require('http');
-const port = 8000;
-var cors = require('cors');
 const accountRouter = require('./routes/account');
 const indexRouter = require('./routes');
 const stockRouter = require('./routes/stocks');
 const userRouter = require('./routes/users');
 const express = require('express');
 const app = express();
-require('dotenv').config();
+require('dotenv').config({
+  path: '../.env',
+});
+const port = process.env.PORT;
+const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
-app.use(express.static('public'));
 app.use(express.json());
+
+let staticPath = '../client/public';
+
+if (process.env.NODE_ENV === 'production') {
+  staticPath = '../client/build';
+}
+app.use(express.static(path.join(__dirname, staticPath)))
+
 app.use(accountRouter);
 app.use(indexRouter);
 app.use(stockRouter);
@@ -20,7 +30,7 @@ app.use(userRouter);
 const server = http.createServer(app);
 
 app.get('*', (req, res) => {
-  res.redirect('/');
+  res.sendFile(path.join(__dirname, staticPath, 'index.html'));
 });
 
 server.listen(port, () => {
